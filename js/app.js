9d779058f1b5c914ae604a891df6ffff60f33e4f
lr222gw <lr222gw@student.lnu.se>
2014-12-10 14:34:13 +0100

    Init

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Lowe on 2014-12-10.
+ */
+"use strict";
+
+
+
+var AppSpace = {
+
+     Init : function(){
+        
+    }
+
+
+
+};
+
+
+
+
+
+
+
+window.onload = function(){
+    AppSpace.Init();
+};
+
